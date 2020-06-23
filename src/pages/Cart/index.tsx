e485b5b3801c645738d32055a77bf35c37352617
willfeliz1 <william_mfelizardo@hotmail.com>
2020-06-23 12:32:08 -0300

    total quantity to cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -56,9 +56,12 @@ const Cart: React.FC = () => {
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalQuantity = products.reduce(
+      (acc, product) => acc + product.quantity,
+      0,
+    );
 
-    return 0;
+    return totalQuantity;
   }, [products]);
 
   return (
