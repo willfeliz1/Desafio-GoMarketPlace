a9b0b0d13d01aa9748da7eb21d1f3ab508161f2c
willfeliz1 <william_mfelizardo@hotmail.com>
2020-06-23 15:42:01 -0300

    show quantity and total value of the items in bottom bar

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const sumCartTotal = products.reduce(
+      (acc, product) => acc + product.quantity * product.price,
+      0,
+    );
 
-    return formatValue(0);
+    return formatValue(sumCartTotal);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const sumItenInCart = products.reduce(
+      (acc, product) => acc + product.quantity,
+      0,
+    );
 
-    return 0;
+    return sumItenInCart;
   }, [products]);
 
   return (
